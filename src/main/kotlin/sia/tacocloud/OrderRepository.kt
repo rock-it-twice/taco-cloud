db39659b27d7aaa6cf6aeddd62583ae83ec80deb
Ilya Smirnov <Rock-it-twice@yandex.ru>
2022-08-05 14:15:28 +0300

    taco-cloud chap3.3

--- src/main/kotlin/sia/tacocloud/OrderRepository.kt
+++ src/main/kotlin/sia/tacocloud/OrderRepository.kt
@@ -1,9 +1,23 @@
 package sia.tacocloud
 
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import java.util.Date
 
 
 // CrudRepository уже содержит все необходимые методы, для манипуляции с хранилищем
 interface OrderRepository : CrudRepository<TacoOrder, Long>{
+
+    // Поиск по Delivery_zip
     fun findByDeliveryZip(deliveryZip: String): List<TacoOrder>
+
+    // Поиск заказов по Delivery_zip внесенных в БД в диапазоне startDate-endDate
+    fun readOrdersByDeliveryZipAndPlacedAtBetween(deliveryZip: String, startDate:Date, endDate: Date): List<TacoOrder>
+
+    //Поиск заказов по имени(deliveryTo) и Городу доставки(deliveryCity) без учёта регистра букв
+    fun findByDeliveryToAndDeliveryCityAllIgnoresCase(deliveryTo: String, deliveryCity: String): List<TacoOrder>
+
+    // Для сложных запросов лучше использовать аннотацию Query
+    @Query("Order o WHERE o.deliveryCity='Seattle'")
+    fun readOrdersDeliveredInSeattle():List<TacoOrder>
 }
